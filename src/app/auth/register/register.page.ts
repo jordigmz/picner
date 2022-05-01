fefaa19f610be003a8e282c0ad379b4764670a75
Jorge Gómez Tortosa <gmzjordi@gmail.com>
2022-05-01 15:44:33 +0200

    Diseño página de login y registro terminado

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  user = {
+    username: '',
+    name: '',
+    password: '',
+    email: '',
+    avatar: '',
+    lat: 0,
+    lng: 0
+  };
+  email2 = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register() {
+
+  }
+
+  takePhoto() {
+
+  }
+
+  pickFromGallery() {
+
   }
 
 }
