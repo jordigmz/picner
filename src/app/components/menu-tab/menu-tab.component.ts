d3e2f5379700183b25797a5d285a084c74117271
Jordi Gómez <gmzjordi@gmail.com>
2022-05-26 18:13:08 +0200

    Página areas diseño terminado y botón SOS

--- src/app/components/menu-tab/menu-tab.component.ts
+++ src/app/components/menu-tab/menu-tab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu-tab',
@@ -6,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-tab.component.scss'],
 })
 export class MenuTabComponent implements OnInit {
-  constructor() { }
+  contactSOS = 112;
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {}
 
+  async emergencyCall() {
+    const alert = await this.alertController.create({
+      cssClass: 'emergencyAlert',
+      header: '¿Necesitas ayuda?',
+      message: 'Solicita SOS por defecto al número <strong>112</strong> o al contacto de emergencia establecido.',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          id: 'cancel-button',
+          handler: () => {
+            console.log('Cancela llamar a emergencias');
+          }
+        }, {
+          text: 'Sí',
+          cssClass: 'confirmSOS',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirma llamar a emergencias');
+            window.open('tel:'+this.contactSOS);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
